c570460e428c65dc95a6a44bf5040739a75e81cb
parth chothani <parthchothani560@gmail.com>
2024-07-03 18:28:54 +0530

    .

--- MongoDB/CRUD/controllers/user.js
+++ MongoDB/CRUD/controllers/user.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 const {User} = require('../models/userSchema');
 
 const fetchUser = async (req, res)=>{
@@ -22,11 +24,42 @@ const addUser = async (req, res)=>{
         });
         console.log('user', user);
         const newUser = await user.save();
-        console.log('newUser', newUser);
+        D
+
         res.status(201).json({success : true});
     }catch(error){
         res.status(400).json({ message: error.message });
     }
 }
 
-module.exports = {fetchUser, addUser}
+const updateUser = async (req, res) => {
+    try {
+        const {_id} = req.params;
+        const user = await User.findByIdAndUpdate(_id, req.body);
+        if (!user) {
+            return res.status(404).json({ message: 'User not found' });
+        }
+        console.log('user updated :', user);
+        res.json({
+            success: true,
+            data: user
+        });
+    } catch (error) {
+        res.status(400).json({ message: error.message });
+    }
+};
+
+const removeUser = async (req, res)=>{
+    try{
+        const {_id} = req.params;
+        const user = await User.findByIdAndDelete(_id);
+        if(!user){
+            return res.status(404).json({success : false, data : `cannot find any product with ID ${_id}`});  
+        }
+        return res.status(200).json({success : true, data : user});
+    }catch(error){
+        res.status(400).json({ success : false ,message: error.message });
+    }
+}
+
+module.exports = {fetchUser, addUser, updateUser ,removeUser }
--- MongoDB/CRUD/routers/router.js
+++ MongoDB/CRUD/routers/router.js
@@ -3,5 +3,7 @@ const userController = require('../controllers/user');
 
 router.get('/user/list', userController.fetchUser);
 router.post('/user/add', userController.addUser);
+router.put('/user/update/:_id', userController.updateUser);
+router.delete('/user/delete/:_id', userController.removeUser);
 
 module.exports = router
